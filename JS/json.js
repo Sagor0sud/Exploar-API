0fba6337584f68036f379ee6952545f3d0646017
Sagor0sud <mehedihasan74364@gmail.com>
2022-08-28 12:01:20 +0600

    you

---
+++ JS/json.js
@@ -0,0 +1,13 @@
+const user = { id: 01, name: 'Mehedi', job: 'Web development' };
+// javaScript object notation(json)
+const stringfiled = JSON.stringify(user);
+console.log(user);
+//Output is : { id: 1, name: 'Mehedi', job: 'Web development' }
+console.log(stringfiled);
+//Output is : {"id":1,"name":"Mehedi","job":"Web development"}
+const shop = {
+    owner: 'Alia',
+    address: {
+
+    }
+}
